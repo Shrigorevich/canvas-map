4c86241c4a5e17c223e17b4e777f097accd20426
Roman <shrigorevich@gmail.com>
2020-08-18 20:53:29 +0300

    path fix

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
+        console.log(path.resolve(__dirname, "client", "build", "index.html"));
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
